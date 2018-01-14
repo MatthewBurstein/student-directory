65648d0d8bc247313eff0f2aa47c273f7812582d
Matt Burstein <j.matthew.burstein@gmail.com>
2018-01-14 21:14:35 +0000

    Add user input to create list of students

--- directory.rb
+++ directory.rb
@@ -12,6 +12,19 @@ students = [
   {name: "Joffrey Baratheon", cohort: :november},
   {name: "Norman Bates", cohort: :november}
 ]
+def input_students
+  puts "Please enter the names of the students"
+  puts "To finish, just hit return twice"
+  students = []
+  name = gets.chomp
+  #while the name is not empty, repeat this code
+  while !name.empty? do
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    name = gets.chomp
+  end
+  students
+end
 def print_header
   puts "The students of Villians Academy"
   puts "-------------"
@@ -26,6 +39,7 @@ def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
 #nothing happens until we call the methods
+students = input_students
 print_header
 print(students)
 print_footer(students)
