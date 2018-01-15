580b4ced8a598a42ec8a7a823df8991c07f60eb8
Matt Burstein <j.matthew.burstein@gmail.com>
2018-01-15 18:08:07 +0000

    Add height, nationlity and hobbies to students. Add user input to get these values Make print(), print_header() and print_footer() prettier

--- directory.rb
+++ directory.rb
@@ -9,22 +9,37 @@ def input_students
     puts "Now we have #{students.count} students"
     name = gets.chomp
   end
-  students
+
+  puts "Please provide a little more information:"
+  students.each do |student|
+    puts "What is #{student[:name]}'s height in cm?"
+    student[:height] = gets.chomp.to_i
+    puts "What is #{student[:name]}'s nationality?"
+    student[:nationality] = gets.chomp.to_sym
+    puts "What are #{student[:name]}'s hobbies? (please separate hobbies with commas)"
+    student[:hobbies] = gets.split(",").each { |hobby| hobby.strip!}
+  end
 end
 
+
 def print_header
-  puts "The students of Villians Academy"
-  puts "-------------"
+  puts "\n\n"
+  puts "The students of Villians Academy".center(50)
+  puts "§------------------------------------------------§".center(50)
+  puts "\n"
 end
 
 def print(students)
   students.each_with_index do |student, idx|
-    puts "#{idx + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
+    puts "#{idx + 1}. #{student[:name]}".ljust(15, ".") +
+         "#{student[:cohort]} cohort".rjust(10)
   end
+  puts "\n"
 end
 
 def print_footer(students)
     puts "Overall, we have #{students.count} great students"
+    puts "\n\n"
 end
 
 students = input_students
