7e93e8069ba9a4936b9557863404d2e492a9fb75
Matt Burstein <j.matthew.burstein@gmail.com>
2018-01-15 15:47:17 +0000

    Update print() to use each_with_index

--- directory.rb
+++ directory.rb
@@ -1,17 +1,3 @@
-#Let's put all the students in an array
-students = [
-  {name: "Dr. Hannibal Lecter", cohort: :november},
-  {name: "Darth Vader", cohort: :november},
-  {name: "Nurse Ratched", cohort: :november},
-  {name: "Michael Corleone", cohort: :november},
-  {name: "Alex DeLarge", cohort: :november},
-  {name: "The Wicked Witch of the West", cohort: :november},
-  {name: "Terminator", cohort: :november},
-  {name: "Freddy Krueger", cohort: :november},
-  {name: "The Joker",cohort: :november},
-  {name: "Joffrey Baratheon", cohort: :november},
-  {name: "Norman Bates", cohort: :november}
-]
 def input_students
   puts "Please enter the names of the students"
   puts "To finish, just hit return twice"
@@ -25,20 +11,22 @@ def input_students
   end
   students
 end
+
 def print_header
   puts "The students of Villians Academy"
   puts "-------------"
 end
+
 def print(students)
-  students.each do |student|
-    puts "#{student[:name]} (#{student[:cohort]} cohort)"
+  students.each_with_index do |student, idx|
+    puts "#{idx + 1}. #{student[:name]} (#{student[:cohort]} cohort)"
   end
 end
 
 def print_footer(students)
     puts "Overall, we have #{students.count} great students"
 end
-#nothing happens until we call the methods
+
 students = input_students
 print_header
 print(students)
